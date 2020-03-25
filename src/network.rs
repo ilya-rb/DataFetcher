a9ac7ac7b0849b97e9f14bc366924c1f75024f8c
ilya-rb <ilya.ryabchuk@gmail.com>
2020-03-25 23:13:10 +0200

    Small network refactor

--- src/network.rs
+++ src/network.rs
@@ -1,55 +1,66 @@
 use crate::config::Config;
 use crate::config::Endpoint;
 use crate::types::Result;
+use crate::errors::AppErrorType;
+use crate::errors::Error::AppError;
 
 use std::collections::HashMap;
 use std::str::FromStr;
 
-use reqwest::header::HeaderMap;
-use reqwest::header::HeaderName;
-use reqwest::header::HeaderValue;
+use reqwest::header::{
+  HeaderMap,
+  HeaderName,
+  HeaderValue,
+};
 
 pub fn make_http_request(config: &Config, endpoint: &Endpoint) -> Result<String> {
-  use crate::errors::AppErrorType;
-  use crate::errors::Error::AppError;
-
-  use AppErrorType::UnableToCreateHttpClient;
-  use AppErrorType::RequestFetchError;
-  use AppErrorType::ResponseParseError;
-
   let client = reqwest::blocking::Client::builder()
     .default_headers(create_header_map(&config, &endpoint))
     .build()
-    .map_err(|_| AppError(UnableToCreateHttpClient, "Unable to create http client"))?;
+    .map_err(|_| AppError(AppErrorType::UnableToCreateHttpClient, "Unable to create http client"))?;
 
-let result = client
-  .get(&endpoint.url)
-  .send()
-  .map_err(|_| AppError(RequestFetchError, "Error performing http request"))?
-  .text()
-  .map_err(|_| AppError(ResponseParseError, "Error parsing request body"))?;
+    let result = client
+      .get(&endpoint.url)
+      .send();
 
-  Ok(result)
+    if result.is_err() {
+      return Err(AppError(AppErrorType::RequestFetchError, "Error performing http request"));
+    }
+
+    let text = result.unwrap().text();
+    if text.is_err() {
+      return Err(AppError(AppErrorType::ResponseParseError, "Error parsing request body"));
+    }
+
+    Ok(text.unwrap())
 }
 
 fn create_header_map(config: &Config, endpoint: &Endpoint) -> HeaderMap {
-  create_headers(config.requests.headers.as_ref())
-  .into_iter()
-  .chain(create_headers(endpoint.headers.as_ref()))
-  .collect()
+  let glob_headers = create_headers(config.requests.headers.as_ref());
+  let child_headers = create_headers(endpoint.headers.as_ref());
+  
+  glob_headers.into_iter()
+    .chain(child_headers)
+    .collect()
 }
 
 fn create_headers(headers: Option<&HashMap<String, String>>) -> HashMap<HeaderName, HeaderValue> {
-  headers
-  .unwrap_or(&HashMap::new())
-  .iter()
-  .map(|(name, value)| create_header(&name, &value))
-  .collect()
-}
+  let mut result = HashMap::new();
 
-fn create_header(name: &str, value: &str) -> (HeaderName, HeaderValue) {
-  (
-    HeaderName::from_str(name).unwrap(),
-    HeaderValue::from_str(value).unwrap(),
-  )
+  if let Some(headers) = headers.as_ref() {
+    for (k, v) in headers.iter() {
+      let name = HeaderName::from_str(k);
+      let value = HeaderValue::from_str(v);
+
+      // Filter invalid headers
+      if name.is_err() || value.is_err() {
+        warn!("Invalid request header:\nName :: {:?}\nValue :: {:?}", name, value);
+        continue;
+      }
+
+      result.insert(name.unwrap(), value.unwrap());
+    }
+  };
+
+  result
 }
